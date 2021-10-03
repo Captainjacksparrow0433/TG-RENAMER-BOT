8e5ee8efed6195b0c266c719c78d3605aec64610
Captain Jack sparrow <90381786+Captainjacksparrow0433@users.noreply.github.com>
2021-10-03 22:36:53 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -16,7 +16,7 @@ Type /help for more details."""
     UPLOAD_START_VIDEO = "Uploading as video.....📤"
     UPLOAD_START = "Uploading as File.....📤"
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.95GB due to Telegram API limitations.I can't do anything for that 🤷‍♂️."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using [Anonymous](https://t.me/Ns_AnoNymouS)'s bot.**"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using [Abintply](https://t.me/Abintply)'s bot.**"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds."
     NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
     NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription.\nIf you think this is a bug, please contact <a href='https://t.me/Ns_Bot_supporters'>Ns Bot Supporters</a>"
@@ -24,7 +24,7 @@ Type /help for more details."""
     DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
     SAVED_RECVD_DOC_FILE = "Document Downloaded Successfully."
-    CUSTOM_CAPTION_UL_FILE = "@renamer_Ns_bot"
+    CUSTOM_CAPTION_UL_FILE = "@ʟͥxͭʀᷤ ᴊᷟᴀͤᴄᴋ࿐⁩bot"
     NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found."
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
     HELP_USER = """Hai <b><i>{}</i></b>, 
@@ -40,7 +40,7 @@ I am Renamer bot ✍ by <a href='https://t.me/{}'>My Father 👨‍🏫</a>
 4. Reply to that message with <code>/rename_vidoe new name.extension</code>. with custom thumbnail support.\n(uploading as Video)
 
    
-<b>Thanks to <i><a href="https://t.me/Ns_AnoNymous">Anonymous 👨‍🏫</a></i> for his source code. check /about for source code</b>
+<b>Thanks to <i><a href="https://t.me/Abintply">ʟͥxͭʀᷤ ᴊᷟᴀͤᴄᴋ࿐⁩ 👨‍🏫</a></i> for his source code. check /about for source code</b>
 
 --------
 
@@ -55,7 +55,7 @@ Free users only 1 request per 30 minutes.
 The given file name has {num} characters.
 
 <b>Essays Not allowed in Telegram file name!</b>
-©️ <code>@renamer_Ns_bot</code>
+©️ <code>@ʟͥxͭʀᷤ ᴊᷟᴀͤᴄᴋ࿐⁩</code>
 Please short your file name and try again!"""
 
     About = """Hi __{}__,
@@ -64,7 +64,7 @@ Please short your file name and try again!"""
 
 **🧰 Framework:** Pyrogram
 
-**👨‍💻 Developer:** [Anonymous](https://t.me/Ns_AnoNymouS)
+**👨‍💻 Developer:** [ᴅͥᴇͭᴠᷤɪᷟʟͤ](https://t.me/Abintply)
 
 **📮 Channel:** [NS BOT UPDATES](https://t.me/Ns_bot_updates)
 
